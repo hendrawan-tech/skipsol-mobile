41859c7ff12b7402456d6285126236466c4a295a
Hendrawan <hendrawan.dev@gmail.com>
2023-04-16 02:03:56 +0700

    starter code

---
+++ app/src/main/java/com/example/skripsol/auth/Login.kt
@@ -0,0 +1,12 @@
+package com.example.skripsol.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.skripsol.R
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_screen)
+    }
+}
---
+++ app/src/main/java/com/example/skripsol/home/Splash.kt
@@ -0,0 +1,12 @@
+package com.example.skripsol.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.skripsol.R
+
+class Splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen)
+    }
+}
