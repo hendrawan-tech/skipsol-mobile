c4c4d637a4e74ec7ba27794bd3dcc290d413734b
perompak_kode <iqbaltech29@gmail.com>
2023-05-17 14:28:41 +0700

    feat(chat) : sliced chat_sceen recycleView implement

---
+++ app/src/main/java/com/example/skripsol/navbar/Adapter/CardItem.kt
@@ -0,0 +1,4 @@
+package com.example.skripsol.navbar.Adapter
+
+data class CardItem(val imageRes: Int, val title: String, val subtitle: String)
+
--- app/src/main/java/com/example/skripsol/navbar/Chat.kt
+++ app/src/main/java/com/example/skripsol/navbar/Chat.kt
@@ -5,15 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.skripsol.R
+import com.example.skripsol.navbar.Adapter.CardAdapter
+import com.example.skripsol.navbar.Adapter.CardItem
 
 class Chat : Fragment() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: CardAdapter
+    private val itemList = mutableListOf<CardItem>()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.chat_screen, container, false)
+        val view = inflater.inflate(R.layout.chat_screen, container, false)
+        recyclerView = view.findViewById(R.id.recycle_view)
+
+        // Tambahkan item ke daftar
+        for (i in 1..500) {
+            itemList.add(CardItem(R.drawable.img_model_profile_home, "Judul $i", "Deskripsi $i"))
+        }
+
+        // Atur layout manager dan adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        adapter = CardAdapter(itemList)
+        recyclerView.adapter = adapter
+
+        return view
     }
 
 
