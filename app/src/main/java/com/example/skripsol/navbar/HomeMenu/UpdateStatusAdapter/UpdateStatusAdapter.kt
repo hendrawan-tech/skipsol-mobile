d75e6f7f92dc5bc4e12d8d934dade86a4b638736
perompak_kode <iqbaltech29@gmail.com>
2023-05-19 05:45:54 +0700

    feat(login) : update UI Login Sliced

--- app/src/main/java/com/example/skripsol/navbar/HomeMenu/UpdateStatusAdapter/UpdateStatusAdapter.kt
+++ app/src/main/java/com/example/skripsol/navbar/HomeMenu/UpdateStatusAdapter/UpdateStatusAdapter.kt
@@ -3,7 +3,6 @@ package com.example.skripsol.navbar.HomeMenu.UpdateStatusAdapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.skripsol.R
