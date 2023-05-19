5d3a40bb27bcf9300e46c0f70db7a4ad5f266cdc
perompak_kode <iqbaltech29@gmail.com>
2023-05-19 21:48:37 +0700

    feat(login) : re-sliced login layout adding password toggle button and fix card Radius only top

--- app/src/main/java/com/example/skripsol/auth/Login.kt
+++ app/src/main/java/com/example/skripsol/auth/Login.kt
@@ -3,14 +3,26 @@ package com.example.skripsol.auth
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.SharedPreferences
+import android.graphics.Outline
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
+import android.text.TextWatcher
+import android.text.method.PasswordTransformationMethod
+import android.view.View
+import android.view.ViewOutlineProvider
 import android.widget.EditText
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.widget.AppCompatButton
+import androidx.core.content.ContextCompat
+import androidx.core.widget.addTextChangedListener
 import com.example.skripsol.R
 import com.example.skripsol.config.Network
 import com.example.skripsol.state.MyState
+import com.google.android.material.card.MaterialCardView
+import com.google.android.material.textfield.TextInputLayout
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -20,10 +32,51 @@ class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_screen)
+
+
         supportActionBar?.hide()
         val button: AppCompatButton = findViewById(R.id.btn_login)
         val email: EditText = findViewById(R.id.EditText_login_nim)
         val password: EditText = findViewById(R.id.EditText_login_password)
+        val passwordTextInputLayout : TextInputLayout = findViewById(R.id.InputLayout_login_password)
+//        CardView
+        val card : MaterialCardView = findViewById(R.id.login_material_card_layout)
+        val cornerRadius = 32F
+
+//      Untuk Set Icon Di End Gravity agar dapat berganti-ganti type
+        passwordTextInputLayout.setEndIconOnClickListener {
+            password.transformationMethod = if (password.transformationMethod == PasswordTransformationMethod.getInstance()) {
+                null // Password terlihat, ubah ke tipe input teks biasa
+            } else {
+                PasswordTransformationMethod.getInstance() // Password tersembunyi, ubah ke tipe input password tersembunyi
+            }
+
+            passwordTextInputLayout.endIconDrawable = ContextCompat.getDrawable(this, if (password.transformationMethod == PasswordTransformationMethod.getInstance()) {
+                R.drawable.icon_toggle_password_false // Ikon mata terlihat (password tersembunyi)
+            } else {
+                R.drawable.icon_toggle_password_true // Ikon mata tersembunyi (password terlihat)
+            })
+
+            password.setSelection(password.text.length)
+        }
+
+
+
+
+//        Fungsi Untuk Rounded Card Di Top nya saja
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            card.outlineProvider = object : ViewOutlineProvider(){
+                @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+                override fun getOutline(view: View?, outline: Outline?){
+                    outline?.setRoundRect(0,0,view!!.width,(view.height+cornerRadius).toInt(), cornerRadius)
+                }
+            }
+            card.clipToOutline = true
+        }
+
+
+
+
 
         button.setOnClickListener {
             login(email.text.toString(), password.text.toString())
