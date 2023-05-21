255bc7728975adb853fdd118ba3850e2ec89395a
perompak_kode <iqbaltech29@gmail.com>
2023-05-21 08:40:43 +0700

    feat (input_judul_ta) : fix scrolled  field abstrak TA

--- app/src/main/java/com/example/skripsol/navbar/HomeMenu/InputJudulTA.kt
+++ app/src/main/java/com/example/skripsol/navbar/HomeMenu/InputJudulTA.kt
@@ -1,11 +1,16 @@
 package com.example.skripsol.navbar.HomeMenu
 
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View.OnTouchListener
+import android.widget.EditText
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.skripsol.FunctionHelper.Get
 import com.example.skripsol.R
+import com.google.android.material.textfield.TextInputLayout
 
 
 class InputJudulTA : AppCompatActivity() {
@@ -15,11 +20,40 @@ class InputJudulTA : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.input_judul_ta_screen)
 
+        val textInputJudulTA :TextInputLayout = findViewById(R.id.InputLayout_input_judul_ta)
+        val editTextJudulTA : EditText = findViewById(R.id.EditText_input_judul_ta)
 
+        val inputLayoutAbstrakTA: TextInputLayout = findViewById(R.id.InputLayout_abstrak_ta)
+        val editTextAbstrakTA: EditText = findViewById(R.id.EditText_abstrak_ta)
+
+        
+
+
+        scrollAbleEdittext(editTextAbstrakTA)
         findViewById<ImageView>(R.id.btn_back_input_judul_ta).setOnClickListener {
-           Get.back(this)
+            Get.back(this)
         }
 
 
+
+
+    }
+
+
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun scrollAbleEdittext(editText : EditText){
+        editText.setOnTouchListener(OnTouchListener { v, event ->
+            if (editText.hasFocus()) {
+                v.parent.requestDisallowInterceptTouchEvent(true)
+                when (event.action and MotionEvent.ACTION_MASK) {
+                    MotionEvent.ACTION_SCROLL -> {
+                        v.parent.requestDisallowInterceptTouchEvent(false)
+                        return@OnTouchListener true
+                    }
+                }
+            }
+            false
+        })
     }
 }
